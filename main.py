3ca981a87000744aa5ccba17c6357307193d5127
spl1c <spl1ce5225@gmail.com>
2021-08-30 22:23:20 +0100

    added rich presence

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='£')
 
-@bot.event
-async def on_connect():
+async def run_once_when_ready():
+    await bot.wait_until_ready()
     print('BTE Italia Bot In Funzione!')
+    await bot.change_presence(activity=discord.Activity(type = discord.ActivityType.watching,name='www.bteitalia.it'))
+
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
 
+bot.loop.create_task(run_once_when_ready())
 bot.run('ODc1MjkzNDA3OTY5NDI3NTE4.YRTagg.59qiBctr7wDaVjXw0hTRtX6zkYs')
