59c4ea006a314ffefd7da77595685a03bb4d86db
spl1c <spl1ce5225@gmail.com>
2021-09-17 02:38:43 +0100

    reazione function added!!!

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,9 +1,10 @@
 import discord
+import emoji
 from discord.ext import commands
 from discord.ext.commands.errors import ChannelNotFound
 
 class Utilities(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot): 
         self.bot = bot
 
     @commands.command(name='post')
@@ -20,12 +21,15 @@ class Utilities(commands.Cog):
     
     @commands.command(name='messaggio')
     @commands.has_role(695697978391789619)
-    async def messaggio(self, ctx, channel, *, message):
+    async def messaggio(self, ctx, channel = None, *, message = None):
         converter = commands.TextChannelConverter()
+        
         if channel != None:
+
             if message != None:
+            
                 try:
-                    destination_channel = converter.convert(ctx, channel)
+                    destination_channel = await converter.convert(ctx, channel)
 
                     embed = discord.Embed(description='Message sent!', color=discord.Color.green())
                     await destination_channel.send(message)
@@ -39,11 +43,64 @@ class Utilities(commands.Cog):
                 await ctx.send(embed=embed)
         
         else:
-            embed = discord.Embed(description='Please provide a valid channel.', color=discord.Color.red())
+            embed = discord.Embed(description='You must provide a channel.', color=discord.Color.red())
             await ctx.send(embed=embed)
     
+    @commands.command(name='reazione')
+    @commands.has_role(695697978391789619)
+    async def reazione(self, ctx, message = None, reaction = None):
+        if message != None:
+        
+            if reaction != None:
+                reaction_message = None
+                try:
+                    message_converter = commands.MessageConverter()
+                    reaction_message = await message_converter.convert(ctx, message)
+
+                except commands.MessageNotFound:
+                    channels = ctx.guild.channels
+                    for channel in channels:
+                        try:
+                            reaction_message = await channel.fetch_message(message)
+
+                        except:
+                            pass
+
+                    if reaction_message == None:
+                        embed = discord.Embed(description="Couldn't find the message.", color=discord.Color.red())
+                        await ctx.send(embed=embed)
+                        return
+
+                if reaction in emoji.UNICODE_EMOJI_ENGLISH:
+                    reaction_emoji = reaction
+
+                else:
+                    try:
+                        emoji_converter = commands.PartialEmojiConverter()
+                        reaction_emoji = await emoji_converter.convert(ctx, reaction)
+                        
+
+
+                    except commands.PartialEmojiConversionFailure:
+                        embed=discord.Embed(description='Please provide a valid emoji.', color=discord.Color.red())
+                        await ctx.send(embed=embed)
+                        return
+
+                await message.add_reaction(reaction_emoji)
+                
+
+            else:
+                embed = discord.Embed(description='You must provide a reaction emoji.', color=discord.Color.red())
+                await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(description='You must provide a message.', color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+
+
     @post.error
     @messaggio.error
+    @reazione.error
     async def handler(self, ctx, error):
         if isinstance(error, commands.MissingRole):
             embed = discord.Embed(description="Non hai il permesso di usare questo comando.", color=discord.Color.red())
