23e5638afff36e4374471b32e1c0293d08dd09ce
rospino74 <34315725+rospino74@users.noreply.github.com>
2021-12-29 15:02:36 +0100

    Moved approva outside of utilities

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,21 +1,10 @@
 import discord
 import emoji
 from discord.ext import commands
-from os import environ
-from utils.spreadsheet import Spreadsheet
-from asyncio import sleep
-
-sh = Spreadsheet(environ.get('SPREADSHEET_ID'))
-
-
-async def refresh_spreadsheet():
-    while True:
-        sh.fetch()
-        await sleep(10800)  # 3 hours
 
 
 class Utilities(commands.Cog):
-    def __init__(self, bot): 
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='post')
@@ -28,60 +17,62 @@ class Utilities(commands.Cog):
         notifiche_channel = self.bot.get_channel(697169688005836810)
 
         await notifiche_channel.send(f'{message}\n\n{link_msg}')
-        
-        embed = discord.Embed(description='Postato!', color=discord.Color.green())
+
+        embed = discord.Embed(description='Postato!',
+                              color=discord.Color.green())
         await ctx.send(embed=embed)
 
-
-    
     @commands.command(name='messaggio')
     @commands.has_role(695697978391789619)
-    async def messaggio(self, ctx, channel = None, *, message = None):
+    async def messaggio(self, ctx, channel=None, *, message=None):
         converter = commands.TextChannelConverter()
-        
+
         if channel != None:
 
             if message != None or ctx.message.attachments != None:
-            
+
                 try:
                     destination_channel = await converter.convert(ctx, channel)
 
-                    
                     try:
                         attachments = ctx.message.attachments
-                        files = []                        
+                        files = []
                         for file in attachments:
-                            files.append(await file.to_file())    
-                        
+                            files.append(await file.to_file())
+
                         await destination_channel.send(content=message, files=files)
 
                     except Exception as e:
-                        embed = discord.Embed(description="Non è stato possibile mandare un messaggio a questo canale.", color=discord.Color.red())
+                        embed = discord.Embed(
+                            description="Non è stato possibile mandare un messaggio a questo canale.", color=discord.Color.red())
                         await ctx.send(embed=embed)
                         print(e)
                         return
 
-                    embed = discord.Embed(description='Messaggio inviato!', color=discord.Color.green())
+                    embed = discord.Embed(
+                        description='Messaggio inviato!', color=discord.Color.green())
                     await ctx.send(embed=embed)
-                
+
                 except commands.ChannelNotFound:
-                    embed = discord.Embed(description='Canale non trovato.', color=discord.Color.red())
+                    embed = discord.Embed(
+                        description='Canale non trovato.', color=discord.Color.red())
                     await ctx.send(embed=embed)
 
             else:
-                embed = discord.Embed(description='Perfavore invia un messaggio valido.', color=discord.Color.red())
+                embed = discord.Embed(
+                    description='Perfavore invia un messaggio valido.', color=discord.Color.red())
                 await ctx.send(embed=embed)
-        
-        else:
-            embed = discord.Embed(description='Devi indicare un canale.', color=discord.Color.red())
-            await ctx.send(embed=embed)
 
+        else:
+            embed = discord.Embed(
+                description='Devi indicare un canale.', color=discord.Color.red())
+            await ctx.send(embed=embed)
 
     @commands.command(name='reazione')
     @commands.has_role(695697978391789619)
-    async def reazione(self, ctx, message = None, reaction = None):
+    async def reazione(self, ctx, message=None, reaction=None):
         if message != None:
-        
+
             if reaction != None:
                 reaction_message = None
                 try:
@@ -98,7 +89,8 @@ class Utilities(commands.Cog):
                             pass
 
                     if reaction_message == None:
-                        embed = discord.Embed(description="Non è stato possibile trovare il messaggio.", color=discord.Color.red())
+                        embed = discord.Embed(
+                            description="Non è stato possibile trovare il messaggio.", color=discord.Color.red())
                         await ctx.send(embed=embed)
                         return
 
@@ -109,65 +101,70 @@ class Utilities(commands.Cog):
                     try:
                         emoji_converter = commands.PartialEmojiConverter()
                         reaction_emoji = await emoji_converter.convert(ctx, reaction)
-                        
-
 
                     except commands.PartialEmojiConversionFailure:
-                        embed = discord.Embed(description='Perfavore indica un emoji valida.', color=discord.Color.red())
+                        embed = discord.Embed(
+                            description='Perfavore indica un emoji valida.', color=discord.Color.red())
                         await ctx.send(embed=embed)
                         return
 
                 try:
                     await reaction_message.add_reaction(reaction_emoji)
-                    
 
                 except Exception as e:
                     print(e)
 
-                    embed = discord.Embed(description="Non è stato possibile reagire al messaggio.", color=discord.Color.red())
+                    embed = discord.Embed(
+                        description="Non è stato possibile reagire al messaggio.", color=discord.Color.red())
                     await ctx.send(embed=embed)
                     return
-                
-                embed = discord.Embed(description='Reagito!', color=discord.Color.green())
-                await ctx.send(embed=embed)    
+
+                embed = discord.Embed(
+                    description='Reagito!', color=discord.Color.green())
+                await ctx.send(embed=embed)
 
             else:
-                embed = discord.Embed(description='Devi indicare un emoji da reagire.', color=discord.Color.red())
+                embed = discord.Embed(
+                    description='Devi indicare un emoji da reagire.', color=discord.Color.red())
                 await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(description='Devi indicare un messaggio.', color=discord.Color.red())
+            embed = discord.Embed(
+                description='Devi indicare un messaggio.', color=discord.Color.red())
             await ctx.send(embed=embed)
 
-    @commands.command(name='approva')
+    @commands.command(name='oldApprova')
     @commands.has_role(704338128692838533)
-    async def approva(self, ctx, member = None):
+    async def approva(self, ctx, member=None):
         approva_channel = ctx.guild.get_channel(891675282992431154)
         if ctx.channel == approva_channel:
             if member != None:
                 try:
-                    converter=commands.MemberConverter()
+                    converter = commands.MemberConverter()
                     member = await converter.convert(ctx, member)
 
                     starter_role = ctx.guild.get_role(704332197628477450)
                     if not starter_role in member.roles:
                         try:
-                            newbie_role = ctx.guild.get_role(884464061851521065)
+                            newbie_role = ctx.guild.get_role(
+                                884464061851521065)
                             await member.remove_roles(newbie_role)
-                            
+
                         except Exception as e:
                             print(e)
-                            embed=discord.Embed(description="Non è stato possibile rimuovere il ruolo Newbie dal utente.", color=discord.Color.red())
+                            embed = discord.Embed(
+                                description="Non è stato possibile rimuovere il ruolo Newbie dal utente.", color=discord.Color.red())
                             await ctx.send(embed=embed)
                             return
 
                         try:
-                            starter_role = ctx.guild.get_role(704332197628477450)
+                            starter_role = ctx.guild.get_role(
+                                704332197628477450)
                             await member.add_roles(starter_role)
-
 
                         except Exception as e:
                             print(e)
-                            embed=discord.Embed(description="Rimosso il ruolo Newbie ma non è stato posssibile assegnare Starter.", color=discord.Color.red())
+                            embed = discord.Embed(
+                                description="Rimosso il ruolo Newbie ma non è stato posssibile assegnare Starter.", color=discord.Color.red())
                             await ctx.send(embed=embed)
                             return
 
@@ -196,16 +193,19 @@ class Utilities(commands.Cog):
                             await ctx.send(embed=embed)
 
                     else:
-                        embed = discord.Embed(description='Utente ha già il ruolo Starter.', color=discord.Color.red())
+                        embed = discord.Embed(
+                            description='Utente ha già il ruolo Starter.', color=discord.Color.red())
                         await ctx.send(embed=embed)
 
                 except commands.MemberNotFound:
-                    embed = discord.Embed(description='Utente non trovato!', color=discord.Color.red())
+                    embed = discord.Embed(
+                        description='Utente non trovato!', color=discord.Color.red())
                     await ctx.send(embed=embed)
                     return
 
             else:
-                embed = discord.Embed(description='Devi indicare un Utente!', color=discord.Color.red())
+                embed = discord.Embed(
+                    description='Devi indicare un Utente!', color=discord.Color.red())
                 await ctx.send(embed=embed)
 
         else:
@@ -217,7 +217,8 @@ class Utilities(commands.Cog):
     @approva.error
     async def handler(self, ctx, error):
         if isinstance(error, commands.MissingRole):
-            embed = discord.Embed(description="Non hai il permesso di usare questo comando.", color=discord.Color.red())
+            embed = discord.Embed(
+                description="Non hai il permesso di usare questo comando.", color=discord.Color.red())
             await ctx.send(embed=embed)
         else:
             print(error)
@@ -225,4 +226,3 @@ class Utilities(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
-    bot.loop.create_task(refresh_spreadsheet())
