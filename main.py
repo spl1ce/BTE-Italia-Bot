899e61125b10c83c9d4cb54155e678ff0925be5c
rospino74 <34315725+rospino74@users.noreply.github.com>
2021-12-30 13:14:13 +0100

    Improved logging

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
-import os
 from discord.ext import commands
 import logging
+import os
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s » %(message)s')
 
 intents = discord.Intents.all()
 intents.reactions = True
@@ -15,15 +16,17 @@ bot = commands.Bot(command_prefix='£', intents=intents)
 async def run_once_when_ready():
     await bot.wait_until_ready()
     print('BTE Italia Bot In Funzione!')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='bteitalia.it'))
+    bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name='bteitalia.it'))
 
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        logging.info(f"{filename} caricato!")
 
     else:
-        print(f'Unable to load {filename}.')
+        logging.error(f"Unable to load {filename}.")
 
 bot.loop.create_task(run_once_when_ready())
 bot.run(os.environ.get('TOKEN'))
