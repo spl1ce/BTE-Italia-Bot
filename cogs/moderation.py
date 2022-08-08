2eea87570d6675e5c70859361a0881bbf5d45967
BTE Italia <71638958+BTEItalia@users.noreply.github.com>
2022-08-08 13:27:34 +0200

    Added command £banlist

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,7 @@
 import discord
 import time
 from discord.ext import commands
+import asyncio
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
@@ -28,6 +29,54 @@ class Moderation(commands.Cog):
             await ctx.channel.send(embed=embed)
         else:
             print(error)
+    
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def banlist(self, ctx):
+        page = 1
+        bans = await ctx.guild.bans()
+
+        def check(reaction, user):
+            return user != self.client.user
+        message = None
+
+        while True:
+            header = f"Lista Utenti Bannati (Pagina #{page}):"
+            embed = discord.Embed(
+                title=f"{header}", color=discord.Color.blue())
+            for ban_entry in bans[(page-1)*10:page*10]:
+                embed.add_field(
+                    name=f"**{ban_entry.user}**", value=f"Motivo: *{ban_entry.reason}*")
+            if message == None:
+                message = await ctx.send(embed=embed)
+            else:
+                await message.edit(embed=embed)
+            if len(bans) > 10 and not page*10 - len(bans) <= 10:
+                await message.add_reaction("◀️")
+                await message.add_reaction("▶️")
+                try:
+                    reaction, user = await self.client.wait_for('reaction_add', timeout=30.0, check=check)
+                except asyncio.TimeoutError:
+                    await message.clear_reaction("◀️")
+                    await message.clear_reaction("▶️")
+                    break
+                if str(reaction) == "▶️":
+                    page += 1
+                elif str(reaction) == "◀️" and page > 1:
+                    page -= 1
+                else:
+                    print("none of the reactions")
+            else:
+                break
+
+    @banlist.error
+    async def unban_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description='Non hai il permesso di eseguire questo comando',
+                colour=discord.Colour.red()
+            )
+            await ctx.send(embed=embed)
             
 def setup(bot):
     bot.add_cog(Moderation(bot))
